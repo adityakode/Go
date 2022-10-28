485edad1018d0df6741f9308a6f6c7e4058ceca9
Aditya Kode <105551807+adityakode@users.noreply.github.com>
2022-10-28 13:07:16 +0530

    corrected

--- 06time/time.go
+++ 06time/time.go
@@ -13,7 +13,7 @@ func main() {
 	fmt.Println(presentTime)
 
 	// this shows the present time
-	// but it looks too long . to print only time and date, we use format
+	// but it looks too long . to print only Dayand date, we use format
 	fmt.Println(presentTime.Format("01-02-2006 Monday"))
 	// to use time , you have to use a specific no in format that is 15:04:05
 	// specific date is 01-02-2006
