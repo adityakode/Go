1d9fdf31e3633981708a1021b872f2696e656d6f
Aditya Kode <105551807+adityakode@users.noreply.github.com>
2022-10-28 11:01:55 +0530

    Add files via upload

---
+++ 06time/time.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Time in golang")
+
+	//for this, we use time package/library
+	presentTime := time.Now()
+	fmt.Println(presentTime)
+
+	// this shows the present time
+	// but it looks too long . to print only time and date, we use format
+	fmt.Println(presentTime.Format("01-02-2006 Monday"))
+	// to use time , you have to use a specific no in format that is 15:04:05
+	// specific date is 01-02-2006
+}
